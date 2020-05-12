a54b2eba40fb1b5e748e33a1d33a193501feb374
Inna <inna.barantseva@volvocars.com>
2020-05-13 02:11:55 +0300

    added solving quadratic equations with complex roots

--- quadratic_equation.hs
+++ quadratic_equation.hs
@@ -1,28 +1,41 @@
 module Main where
 import Data.List
+import Data.Complex
+import Data.Either
 
-countD :: Float -> Float -> Float -> Float 
-countD a b c = b*b - 4*a*c
+countD :: Float -> Float -> Float -> Float
+countD 0 0 0 = 0
+countD a b c = b * b - 4 * a * c
 
-countX :: Float -> Float -> Float -> [[Float]]
-countX d a b = do
-	if d < 0 then
-		return [0/0]
-	else
-		if d == 0 then do
-			let x = ( (-1) * b) / (2 * a)
-			return [x, x]
-		else do
-			let x1 = ( (-1) * b + sqrt(d) ) / (2 * a)
-			let x2 = ( (-1) * b - sqrt(d) ) / (2 * a)
-			return [x1, x2]
+countComplexSqrtD :: Float -> Complex Float
+countComplexSqrtD 0 = (0.0 :+ 0.0)
+countComplexSqrtD n = sqrt (n :+ 0.0)
 
+countComplexX :: Float -> Float -> Complex Float -> [Complex Float]
+countComplexX a b sd = [x1, x2]
+  where x1 = ((-1) * (b :+ 0.0) + sd ) / (2 * (a :+ 0.0))
+        x2 = ((-1) * (b :+ 0.0) - sd ) / (2 * (a :+ 0.0))
+       
+
+countX :: Float -> Float -> Float -> [Float]
+countX 0 0 0 = []
+countX a b c | d <  0    = []
+             | d == 0    = [x]
+             | otherwise = [x1, x2]
+  where d  = b * b - 4 * a * c
+        x  = ((-1) * b) / (2 * a)
+        x1 = ((-1) * b + sqrt(d) ) / (2 * a) 
+        x2 = ((-1) * b - sqrt(d) ) / (2 * a)
 
 main :: IO ()
 main = do
 	let a = 1
-	let b = 7
-	let c = 2
+	let b = 0
+	let c = 1
 
-	let d = countD a b c
-	putStr $ unlines $ map (unwords . map show) $ countX d a b 
+	let d = countD a b c 
+	if d < 0 then 
+		let sd = countComplexSqrtD d in
+		print $ countComplexX a b sd
+	else 
+		print $ countX a b c
